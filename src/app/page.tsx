7ac16a5c58253d9d53679e0742dc65f57c0d8e8d
Anmol <anmolsaxena456@gmail.com>
2024-03-25 18:00:40 -0500

    baseline react app

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div>
+			<div id="upload">
+				hi
+			</div>
+			
+			<div id="view">
+				hi
+			</div>
+		</div>
+	);
+}
